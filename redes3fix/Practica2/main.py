3e27470b677b43d475e37b04ac73894572e80cd9
Juan Carlos <carlos_isc7@outlook.com>
2019-03-12 14:11:48 -0600

    GUI added and other changes

--- redes3fix/Practica2/main.py
+++ redes3fix/Practica2/main.py
@@ -1,33 +1,58 @@
 import handlerSNMP as hs
+from six.moves import tkinter as tk
+
+class GUI(tk.Frame):
+    def __init__(self, parent = None):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.init_ui()
+
+        self.commmnity_lbl = tk.Label(self.parent, text = "Comunidad")
+
+        self.commmnity_lbl.pack()
+        self.commmnity_lbl.place(relx=.1, rely=.1, anchor="c")
+
+        self.com_text = tk.Entry(self.parent)
+        self.com_text.insert(0, "gr_4cm1")
+        self.com_text.pack()
+        self.com_text.place(relx=.3, rely = 0.1, anchor = "c")
 
 
-info_rrdtool = {"path": "/home/linuxsnmp/Escritorio/",
-                "name": "trend.rrd"}
+        self.ip_lbl = tk.Label(self.parent, text = "IP:")
+        self.ip_lbl.pack()
+        self.ip_lbl.place(relx =.1, rely = 0.15, anchor = "c")
 
-community = input("Comunidad: ")
-ip        = input("IP: ")
+        self.ip_text = tk.Entry(self.parent)
+        self.ip_text.insert(0, "localhost")
+        self.ip_text.pack()
+        self.ip_text.place(relx = .3, rely = .15, anchor = "c")
 
+        self.btn_start = tk.Button(self.parent, text = "Start", command = self.start)
+        self.btn_start.pack()
+        self.btn_start.place(relx=0.1, rely=.25, anchor="c")
 
-threshold_breakpoint = input("threshold Breakpoint: ")
-threshold_upper = input("threshold Upper: ")
-threshold_lowe  = input("threshold Lower: ")
+        #self. = tk.
 
+    def init_ui(self):
+        self.parent.title("Monitoreo")
+    def start(self):
+        path_rrd = "/home/linuxsnmp/Escritorio/"
+        name_rrd = "trend.rrd"
 
+        h1 = hs.HandlerSNMP(path_rrd, name_rrd)
 
+        h1.create()
 
+        commmnity = self.com_text.get()
+        ip = self.ip_text.get()
 
-h1 = hs.HandlerSNMP(info_rrdtool["path"],
-                    info_rrdtool["name"])
+        h1.update(commmnity, ip, OID = 'iso.3.6.1.2.1.25.3.3.1.2.196608')
+        h1.create_image(path_rrd)
 
-h1.create()
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("600x400")
+    app = GUI(parent = root)
+    app.mainloop()
+    root.destroy()
 
-h1.update(community, ip)
-h1.create_image(info_rrdtool["path"],
-                threshold_lowe,
-                threshold_upper,
-                threshold_breakpoint
-                )
-
-
-def menu():
-    print("")
