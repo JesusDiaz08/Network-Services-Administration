582f40296541fdfe346d61e82529a540f7032e47
Juan Carlos <carlos_isc7@outlook.com>
2019-03-13 02:11:07 -0600

    modified to also send rrd file

--- redes3fix/Practica2/Notify.py
+++ redes3fix/Practica2/Notify.py
@@ -1,7 +1,8 @@
 import smtplib
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-
+from email.mime.base import MIMEBase
+from email import encoders
 COMMASPACE = ', '
 # Define params
 
@@ -14,7 +15,7 @@ mailserver = 'smtp.gmail.com: 587'
 #password = 'Secreto123'
 password = "contraredes3"
 
-def send_alert_attached(subject, pngpath, pngname):
+def send_alert_attached(subject, pngpath, pngname, name):
     """ Will send e-mail, attaching png
     files in the flist.
     """
@@ -23,9 +24,22 @@ def send_alert_attached(subject, pngpath, pngname):
     msg['From'] = mailsender
     msg['To'] = mailreceip
     fp = open(pngpath+pngname, 'rb')
+
+    rrd_file = open(pngpath + name, "rb")
+    p = MIMEBase("application", "octet-stream")
+    p.set_payload((rrd_file).read())
+    encoders.encode_base64(p)
+    p.add_header("Content-Disposition", "attachment", filename = name)
+
     img = MIMEImage(fp.read())
+    img.add_header("Content-Disposition", "attachment", filename = pngname)
+    rrd_file.close()
     fp.close()
+
     msg.attach(img)
+    msg.attach(p)
+
+
     mserver = smtplib.SMTP(mailserver)
     mserver.starttls()
     # Login Credentials for sending the mail
